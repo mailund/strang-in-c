87451086d3fa1a7ebec319a73a3b1636d782b1e9
Thomas Mailund <tm@kvantify.dk>
2025-02-16 07:24:09 +0100

    Variant of naive

--- src/search.c
+++ src/search.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 const char *naive_search(const char *x, const char *p) {
-  for (const char *i = x; *i != '\0'; i++) {
-    for (const char *j = p; /* check in body*/; i++, j++)
+  for (; *x; x++) {
+    for (const char *i = x, *j = p; /* check in body*/; i++, j++)
       if (*j == '\0') // We reached the end of p
         return i;
       else if (*i != *j) // We hit a mismatch
